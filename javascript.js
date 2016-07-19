60e5f7e0d3955b8b0c15243f5315f9a077fb2f7f
marlene <marleneguerrero33@gmail.com>
2016-07-19 11:22:19 -0700

    fixed the scroll AGAIN

--- javascript.js
+++ javascript.js
@@ -3,11 +3,13 @@ $("#nbabout").click(function() {
         scrollTop: $("#about").offset().top
     }, 2000);
 });
+
 $("#dailypositivity").click(function() {
     $('html, body').animate({
         scrollTop: $("#daily").offset().top
     }, 2000);
 });
+
 $("#game1").click(function() {
     $('html, body').animate({
         scrollTop: $("#about").offset().top
@@ -19,7 +21,8 @@ $("#top").click(function() {
         scrollTop: $("#header").offset().top
     }, 2000);
 });
-
-function expand(){
-	document.getElementById("about").innerHTML = "About"
-}
+$("#helpfullinks").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#helpful").offset().top
+    }, 2000);
+});
