794902a4747fc2099312e87238c07740a82d4754
marlene <marleneguerrero33@gmail.com>
2016-07-27 13:21:34 -0700

    added footer

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -26,3 +26,32 @@ $("#helpfullinks").click(function() {
         scrollTop: $("#helpful").offset().top
     }, 2000);
 });
+$("#about5").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 2000);
+});
+
+$("#dailypositivity5").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#daily").offset().top
+    }, 2000);
+});
+
+$("#game5").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#game").offset().top
+    }, 2000);
+});
+
+$("#top5").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#game1").offset().top
+    }, 2000);
+});
+$("#helpfullinks5").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#helpful").offset().top
+    }, 2000);
+});
+
